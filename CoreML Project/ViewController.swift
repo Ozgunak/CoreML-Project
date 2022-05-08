11a388cff5a1711cb527c3fcf2b3bf048958ad84
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2022-05-08 14:14:54 +0300

    Initial Commit

---
+++ CoreML Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreML Project
+//
+//  Created by ozgun on 8.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
